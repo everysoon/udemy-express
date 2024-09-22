0cfabe826d6861126a21ff50f02040a4cc87404c
everysoon <minsoon1269@naver.com>
2024-09-22 23:43:18 +0900

    [ADD] db config env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const AppError = require('./util/error');
+const mysql = require("mysql");
+const AppError = require("./util/error");
 
 // const morgan = require('morgan');
 // 1. middleware
@@ -17,11 +17,11 @@ app.use((req, res, next) => {
 });
 // 2. api route handle
 // 3. router setting
-app.use('/api/v1/tours', require('./route/tours'));
-app.use('/api/v1/users', require('./route/users'));
+app.use("/api/v1/tours", require("./route/tours"));
+app.use("/api/v1/users", require("./route/users"));
 
 // error handle - 마지막에 있어야함
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   // res.status(400).json({
   //   status: 'fail',
   //   msg: `Can\'t find ${req.originalUrl} on this Server`,
@@ -32,6 +32,7 @@ app.all('*', (req, res, next) => {
   next(new AppError(`Can\'t find ${req.originalUrl} on this Server`, 404));
 });
 // error handle middleware
-app.use(require('./route/error'));
+app.use(require("./route/error"));
 // 4. start server
+
 module.exports = app;
